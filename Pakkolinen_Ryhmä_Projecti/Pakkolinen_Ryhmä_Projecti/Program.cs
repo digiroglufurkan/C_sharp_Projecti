ea1e5924b37ca12ed482ac7a47d60dbf56129a36
ASK259 <antti.kuusisto@edu.keuda.fi>
2022-04-27 14:08:37 +0300

    Admin osiota tehty. Jatkuu myöhemmin tänään lisää.

--- Pakkolinen_Ryhmä_Projecti/Pakkolinen_Ryhmä_Projecti/Program.cs
+++ Pakkolinen_Ryhmä_Projecti/Pakkolinen_Ryhmä_Projecti/Program.cs
@@ -11,7 +11,8 @@ namespace Pakkolinen_Ryhmä_Projecti
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Etusivu());
+            Application.Run(new Etusivu()); 
+            //Application.Run(new AdminKotisivu()); // Tämä on, jotta ohjelma aloittaa pyörimisen Adminin kotisivulta
         }
     }
 }
