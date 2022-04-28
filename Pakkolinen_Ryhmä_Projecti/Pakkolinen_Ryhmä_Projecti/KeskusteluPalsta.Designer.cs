34a19e7bf0a687c844ce062fbd0e717e9e93a116
Okehittaja <Okehittaja@ML-RefVm-640629>
2022-04-28 09:03:11 +0000

    kirjautuneen pohjat

---
+++ Pakkolinen_Ryhmä_Projecti/Pakkolinen_Ryhmä_Projecti/KeskusteluPalsta.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Pakkolinen_Ryhmä_Projecti
+{
+    partial class KeskusteluPalsta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ksEtusivuOtsikkoLB = new System.Windows.Forms.Label();
+            this.ksKotisivuMS = new System.Windows.Forms.MenuStrip();
+            this.ksKeskusteluKotisivuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ksKotisivuLatauksetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ksKotisivuJaaTiedostoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ksKotisivuKeskustelupalstaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ksKotisivuPalauteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ksKotisivuAsetuksetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ksMuokkaaProfiiliaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ksVaihdaSalasanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ksKirjauduUlosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mitaUuttaLB = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ksKommenttiDG = new System.Windows.Forms.DataGridView();
+            this.ksKotisivuMS.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ksKommenttiDG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ksEtusivuOtsikkoLB
+            // 
+            this.ksEtusivuOtsikkoLB.AutoSize = true;
+            this.ksEtusivuOtsikkoLB.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ksEtusivuOtsikkoLB.Location = new System.Drawing.Point(300, 9);
+            this.ksEtusivuOtsikkoLB.Name = "ksEtusivuOtsikkoLB";
+            this.ksEtusivuOtsikkoLB.Size = new System.Drawing.Size(149, 51);
+            this.ksEtusivuOtsikkoLB.TabIndex = 3;
+            this.ksEtusivuOtsikkoLB.Text = "Otsikko";
+            // 
+            // ksKotisivuMS
+            // 
+            this.ksKotisivuMS.Dock = System.Windows.Forms.DockStyle.None;
+            this.ksKotisivuMS.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ksKeskusteluKotisivuToolStripMenuItem,
+            this.ksKotisivuLatauksetToolStripMenuItem,
+            this.ksKotisivuJaaTiedostoToolStripMenuItem,
+            this.ksKotisivuKeskustelupalstaToolStripMenuItem,
+            this.ksKotisivuPalauteToolStripMenuItem,
+            this.ksKotisivuAsetuksetToolStripMenuItem});
+            this.ksKotisivuMS.Location = new System.Drawing.Point(101, 60);
+            this.ksKotisivuMS.Name = "ksKotisivuMS";
+            this.ksKotisivuMS.Size = new System.Drawing.Size(548, 28);
+            this.ksKotisivuMS.TabIndex = 4;
+            // 
+            // ksKeskusteluKotisivuToolStripMenuItem
+            // 
+            this.ksKeskusteluKotisivuToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ksKeskusteluKotisivuToolStripMenuItem.Name = "ksKeskusteluKotisivuToolStripMenuItem";
+            this.ksKeskusteluKotisivuToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.ksKeskusteluKotisivuToolStripMenuItem.Text = "Kotisivu";
+            // 
+            // ksKotisivuLatauksetToolStripMenuItem
+            // 
+            this.ksKotisivuLatauksetToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ksKotisivuLatauksetToolStripMenuItem.Name = "ksKotisivuLatauksetToolStripMenuItem";
+            this.ksKotisivuLatauksetToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.ksKotisivuLatauksetToolStripMenuItem.Text = "Lataukset";
+            // 
+            // ksKotisivuJaaTiedostoToolStripMenuItem
+            // 
+            this.ksKotisivuJaaTiedostoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ksKotisivuJaaTiedostoToolStripMenuItem.Name = "ksKotisivuJaaTiedostoToolStripMenuItem";
+            this.ksKotisivuJaaTiedostoToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.ksKotisivuJaaTiedostoToolStripMenuItem.Text = "Jaa tiedosto";
+            // 
+            // ksKotisivuKeskustelupalstaToolStripMenuItem
+            // 
+            this.ksKotisivuKeskustelupalstaToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ksKotisivuKeskustelupalstaToolStripMenuItem.Name = "ksKotisivuKeskustelupalstaToolStripMenuItem";
+            this.ksKotisivuKeskustelupalstaToolStripMenuItem.Size = new System.Drawing.Size(130, 24);
+            this.ksKotisivuKeskustelupalstaToolStripMenuItem.Text = "Keskustelupalsta";
+            // 
+            // ksKotisivuPalauteToolStripMenuItem
+            // 
+            this.ksKotisivuPalauteToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ksKotisivuPalauteToolStripMenuItem.Name = "ksKotisivuPalauteToolStripMenuItem";
+            this.ksKotisivuPalauteToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.ksKotisivuPalauteToolStripMenuItem.Text = "Palaute";
+            // 
+            // ksKotisivuAsetuksetToolStripMenuItem
+            // 
+            this.ksKotisivuAsetuksetToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ksMuokkaaProfiiliaToolStripMenuItem,
+            this.ksVaihdaSalasanaToolStripMenuItem,
+            this.ksKirjauduUlosToolStripMenuItem});
+            this.ksKotisivuAsetuksetToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ksKotisivuAsetuksetToolStripMenuItem.Name = "ksKotisivuAsetuksetToolStripMenuItem";
+            this.ksKotisivuAsetuksetToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.ksKotisivuAsetuksetToolStripMenuItem.Text = "Asetukset";
+            // 
+            // ksMuokkaaProfiiliaToolStripMenuItem
+            // 
+            this.ksMuokkaaProfiiliaToolStripMenuItem.Name = "ksMuokkaaProfiiliaToolStripMenuItem";
+            this.ksMuokkaaProfiiliaToolStripMenuItem.Size = new System.Drawing.Size(194, 24);
+            this.ksMuokkaaProfiiliaToolStripMenuItem.Text = "Muokkaa profiilia";
+            // 
+            // ksVaihdaSalasanaToolStripMenuItem
+            // 
+            this.ksVaihdaSalasanaToolStripMenuItem.Name = "ksVaihdaSalasanaToolStripMenuItem";
+            this.ksVaihdaSalasanaToolStripMenuItem.Size = new System.Drawing.Size(194, 24);
+            this.ksVaihdaSalasanaToolStripMenuItem.Text = "Vaihda salasana";
+            // 
+            // ksKirjauduUlosToolStripMenuItem
+            // 
+            this.ksKirjauduUlosToolStripMenuItem.Name = "ksKirjauduUlosToolStripMenuItem";
+            this.ksKirjauduUlosToolStripMenuItem.Size = new System.Drawing.Size(194, 24);
+            this.ksKirjauduUlosToolStripMenuItem.Text = "Kirjaudu ulos";
+            // 
+            // mitaUuttaLB
+            // 
+            this.mitaUuttaLB.AutoSize = true;
+            this.mitaUuttaLB.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mitaUuttaLB.Location = new System.Drawing.Point(326, 111);
+            this.mitaUuttaLB.Name = "mitaUuttaLB";
+            this.mitaUuttaLB.Size = new System.Drawing.Size(111, 20);
+            this.mitaUuttaLB.TabIndex = 7;
+            this.mitaUuttaLB.Text = "Jätä kommentti";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 134);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(406, 104);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(329, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 32);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Julkaise";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ksKommenttiDG
+            // 
+            this.ksKommenttiDG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ksKommenttiDG.Location = new System.Drawing.Point(39, 282);
+            this.ksKommenttiDG.Name = "ksKommenttiDG";
+            this.ksKommenttiDG.RowTemplate.Height = 25;
+            this.ksKommenttiDG.Size = new System.Drawing.Size(713, 156);
+            this.ksKommenttiDG.TabIndex = 10;
+            // 
+            // KeskusteluPalsta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ksKommenttiDG);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.mitaUuttaLB);
+            this.Controls.Add(this.ksKotisivuMS);
+            this.Controls.Add(this.ksEtusivuOtsikkoLB);
+            this.Name = "KeskusteluPalsta";
+            this.Text = "KeskusteluPalsta";
+            this.ksKotisivuMS.ResumeLayout(false);
+            this.ksKotisivuMS.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ksKommenttiDG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label ksEtusivuOtsikkoLB;
+        private MenuStrip ksKotisivuMS;
+        private ToolStripMenuItem ksKeskusteluKotisivuToolStripMenuItem;
+        private ToolStripMenuItem ksKotisivuLatauksetToolStripMenuItem;
+        private ToolStripMenuItem ksKotisivuJaaTiedostoToolStripMenuItem;
+        private ToolStripMenuItem ksKotisivuKeskustelupalstaToolStripMenuItem;
+        private ToolStripMenuItem ksKotisivuPalauteToolStripMenuItem;
+        private ToolStripMenuItem ksKotisivuAsetuksetToolStripMenuItem;
+        private ToolStripMenuItem ksMuokkaaProfiiliaToolStripMenuItem;
+        private ToolStripMenuItem ksVaihdaSalasanaToolStripMenuItem;
+        private ToolStripMenuItem ksKirjauduUlosToolStripMenuItem;
+        private Label mitaUuttaLB;
+        private TextBox textBox1;
+        private Button button1;
+        private DataGridView ksKommenttiDG;
+    }
+}
