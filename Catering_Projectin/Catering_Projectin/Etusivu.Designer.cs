9c231aee285c6adfdf75777eeee877b5c5b1960d
ASK259 <antti.kuusisto@edu.keuda.fi>
2022-05-31 11:16:40 +0300

    Etusivulla kuvat + lähteet.

--- Catering_Projectin/Catering_Projectin/Etusivu.Designer.cs
+++ Catering_Projectin/Catering_Projectin/Etusivu.Designer.cs
@@ -34,12 +34,14 @@
             this.EtusivuTMSI = new System.Windows.Forms.ToolStripMenuItem();
             this.KirjauduTMSI = new System.Windows.Forms.ToolStripMenuItem();
             this.RekisteroidyTMSI = new System.Windows.Forms.ToolStripMenuItem();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Kuv1PB = new System.Windows.Forms.PictureBox();
+            this.Kuv2PB = new System.Windows.Forms.PictureBox();
             this.Kuvaus1RTB = new System.Windows.Forms.RichTextBox();
+            this.Ku1LahLB = new System.Windows.Forms.Label();
+            this.Ku2LahLB = new System.Windows.Forms.Label();
             this.NavMS.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kuv1PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kuv2PB)).BeginInit();
             this.SuspendLayout();
             // 
             // OtsikkoLB
@@ -63,7 +65,7 @@
             this.RekisteroidyTMSI});
             this.NavMS.Location = new System.Drawing.Point(308, 103);
             this.NavMS.Name = "NavMS";
-            this.NavMS.Size = new System.Drawing.Size(524, 39);
+            this.NavMS.Size = new System.Drawing.Size(374, 39);
             this.NavMS.TabIndex = 1;
             this.NavMS.Text = "menuStrip1";
             // 
@@ -88,41 +90,67 @@
             this.RekisteroidyTMSI.Text = "Rekisteröidy";
             this.RekisteroidyTMSI.Click += new System.EventHandler(this.RekisteroidyTMSI_Click);
             // 
-            // pictureBox1
+            // Kuv1PB
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(110, 204);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(289, 231);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.Kuv1PB.Image = ((System.Drawing.Image)(resources.GetObject("Kuv1PB.Image")));
+            this.Kuv1PB.Location = new System.Drawing.Point(28, 165);
+            this.Kuv1PB.Name = "Kuv1PB";
+            this.Kuv1PB.Size = new System.Drawing.Size(495, 348);
+            this.Kuv1PB.TabIndex = 2;
+            this.Kuv1PB.TabStop = false;
             // 
-            // pictureBox2
+            // Kuv2PB
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(640, 204);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(303, 236);
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.Kuv2PB.Image = ((System.Drawing.Image)(resources.GetObject("Kuv2PB.Image")));
+            this.Kuv2PB.Location = new System.Drawing.Point(574, 165);
+            this.Kuv2PB.Name = "Kuv2PB";
+            this.Kuv2PB.Size = new System.Drawing.Size(495, 348);
+            this.Kuv2PB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Kuv2PB.TabIndex = 3;
+            this.Kuv2PB.TabStop = false;
             // 
             // Kuvaus1RTB
             // 
             this.Kuvaus1RTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Kuvaus1RTB.Enabled = false;
-            this.Kuvaus1RTB.Location = new System.Drawing.Point(268, 527);
+            this.Kuvaus1RTB.Location = new System.Drawing.Point(270, 611);
             this.Kuvaus1RTB.Name = "Kuvaus1RTB";
             this.Kuvaus1RTB.ReadOnly = true;
             this.Kuvaus1RTB.Size = new System.Drawing.Size(567, 171);
             this.Kuvaus1RTB.TabIndex = 4;
             this.Kuvaus1RTB.Text = resources.GetString("Kuvaus1RTB.Text");
             // 
+            // Ku1LahLB
+            // 
+            this.Ku1LahLB.AutoSize = true;
+            this.Ku1LahLB.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Ku1LahLB.Location = new System.Drawing.Point(12, 528);
+            this.Ku1LahLB.Name = "Ku1LahLB";
+            this.Ku1LahLB.Size = new System.Drawing.Size(519, 17);
+            this.Ku1LahLB.TabIndex = 5;
+            this.Ku1LahLB.Text = "Lähde: https://commons.wikimedia.org/wiki/File:DO_%26_CO_Catering_-_8968355090.jp" +
+    "g";
+            // 
+            // Ku2LahLB
+            // 
+            this.Ku2LahLB.AutoSize = true;
+            this.Ku2LahLB.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Ku2LahLB.Location = new System.Drawing.Point(589, 516);
+            this.Ku2LahLB.Name = "Ku2LahLB";
+            this.Ku2LahLB.Size = new System.Drawing.Size(467, 17);
+            this.Ku2LahLB.TabIndex = 6;
+            this.Ku2LahLB.Text = "Lähde: //commons.wikimedia.org/wiki/File:EVA_Air_Premium_Laurel_catering.jpg";
+            // 
             // Etusivu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1129, 772);
+            this.Controls.Add(this.Ku2LahLB);
+            this.Controls.Add(this.Ku1LahLB);
             this.Controls.Add(this.Kuvaus1RTB);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Kuv2PB);
+            this.Controls.Add(this.Kuv1PB);
             this.Controls.Add(this.OtsikkoLB);
             this.Controls.Add(this.NavMS);
             this.MainMenuStrip = this.NavMS;
@@ -130,8 +158,8 @@
             this.Text = "Catering";
             this.NavMS.ResumeLayout(false);
             this.NavMS.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kuv1PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kuv2PB)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,8 +172,10 @@
         private ToolStripMenuItem EtusivuTMSI;
         private ToolStripMenuItem KirjauduTMSI;
         private ToolStripMenuItem RekisteroidyTMSI;
-        private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
+        private PictureBox Kuv1PB;
+        private PictureBox Kuv2PB;
         private RichTextBox Kuvaus1RTB;
+        private Label Ku1LahLB;
+        private Label Ku2LahLB;
     }
 }
